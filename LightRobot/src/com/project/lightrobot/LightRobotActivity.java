6e59121a3cbe0ce3e6b0342d129fa1c80ff93d1a
Oier Mees <oier.mees@gmail.com>
2012-06-08 10:46:46 +0200

    Initial commit 2

---
+++ LightRobot/src/com/project/lightrobot/LightRobotActivity.java
@@ -0,0 +1,13 @@
+package com.project.lightrobot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LightRobotActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
