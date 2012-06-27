6c818876a2ad554a61754f24fc831aba3a2ed431
Julian Wormer <julian@julianwormer.de>
2012-06-27 14:58:22 +0200

    Parser is now fully implemented, but recognized commands do not cause a action at the moment.2

--- src/org/lightrobotremote/LightRobotVoiceControl.java
+++ src/org/lightrobotremote/LightRobotVoiceControl.java
@@ -134,42 +134,91 @@ public class LightRobotVoiceControl {
 				temp_command[0] = possible_commands.get(i).get(0);
 				temp_command[1] = possible_commands.get(i).get(1);
 
-				if(temp_command[0].equalsIgnoreCase(PART_1_0))
+				if(temp_command[0].equalsIgnoreCase(PART_1_0))//move
 				{
 					isFirstPartCorrect = true;
-					if(temp_command[1].equalsIgnoreCase(PART_2_MOVE_0))
+					if(temp_command[1].equalsIgnoreCase(PART_2_MOVE_0))//forward
 					{
 						isSecondPartCorrect = true;
-
 					}
-					else if(temp_command[1].equalsIgnoreCase(PART_2_MOVE_1))
+					else if(temp_command[1].equalsIgnoreCase(PART_2_MOVE_1))//backward
 					{
 						isSecondPartCorrect = true;
-
 					}
-					else if(temp_command[1].equalsIgnoreCase(PART_2_MOVE_2))
+					else if(temp_command[1].equalsIgnoreCase(PART_2_MOVE_2))//random
 					{
 						isSecondPartCorrect = true;
-
 					}
 				}
-				else if(temp_command[0].equalsIgnoreCase(PART_1_1))
+				else if(temp_command[0].equalsIgnoreCase(PART_1_1))//turn
 				{
 					isFirstPartCorrect = true;
-
+					if(temp_command[1].equalsIgnoreCase(PART_2_TURN_0))//left
+					{
+						isSecondPartCorrect = true;
+					}
+					else if(temp_command[1].equalsIgnoreCase(PART_2_TURN_1))//right
+					{
+						isSecondPartCorrect = true;
+					}
 				}
-				else if(temp_command[0].equalsIgnoreCase(PART_1_2))
+				else if(temp_command[0].equalsIgnoreCase(PART_1_2))//shine
 				{
 					isFirstPartCorrect = true;
-
+					if(temp_command[1].equalsIgnoreCase(PART_2_SHINE_0))//white
+					{
+						isSecondPartCorrect = true;
+					}
+					else if(temp_command[1].equalsIgnoreCase(PART_2_SHINE_1))//black
+					{
+						isSecondPartCorrect = true;
+					}
+					else if(temp_command[1].equalsIgnoreCase(PART_2_SHINE_2))//red
+					{
+						isSecondPartCorrect = true;
+					}
+					else if(temp_command[1].equalsIgnoreCase(PART_2_SHINE_3))//green
+					{
+						isSecondPartCorrect = true;
+					}
+					else if(temp_command[1].equalsIgnoreCase(PART_2_SHINE_4))//blue
+					{
+						isSecondPartCorrect = true;
+					}
+					else if(temp_command[1].equalsIgnoreCase(PART_2_SHINE_5))//random
+					{
+						isSecondPartCorrect = true;
+					}
 				}
 				else if(temp_command[0].equalsIgnoreCase(PART_1_3))
 				{
 					isFirstPartCorrect = true;
-
+					if(temp_command[1].equalsIgnoreCase(PART_2_BLINK_0))//white
+					{
+						isSecondPartCorrect = true;
+					}
+					else if(temp_command[1].equalsIgnoreCase(PART_2_BLINK_1))//black
+					{
+						isSecondPartCorrect = true;
+					}
+					else if(temp_command[1].equalsIgnoreCase(PART_2_BLINK_2))//red
+					{
+						isSecondPartCorrect = true;
+					}
+					else if(temp_command[1].equalsIgnoreCase(PART_2_BLINK_3))//green
+					{
+						isSecondPartCorrect = true;
+					}
+					else if(temp_command[1].equalsIgnoreCase(PART_2_BLINK_4))//blue
+					{
+						isSecondPartCorrect = true;
+					}
+					else if(temp_command[1].equalsIgnoreCase(PART_2_BLINK_5))//random
+					{
+						isSecondPartCorrect = true;
+					}
 				}
 			}
-
 
 			mIsFirstPartCorrect = isFirstPartCorrect;
 			mIsSecondPartCorrect = isSecondPartCorrect;
